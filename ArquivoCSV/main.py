9cd2523ba58e2b95ebacaefc1aa6921d1c09c1a1
Corrêa <jovicorreabz@gmail.com>
2026-05-27 18:44:08 -0400

    Update main.py

--- ArquivoCSV/main.py
+++ ArquivoCSV/main.py
@@ -1,18 +1,12 @@
 import pandas as pd
 
-
-df = pd.read_csv("resultado.csv")
+df = pd.read_csv("arquivo-estudo.csv")
 
 df["pontuacao"] = pd.to_numeric(df["pontuacao"], errors="coerce")
-
 df = df.dropna(subset=["pontuacao"])
-
 media_geral = df["pontuacao"].mean()
-
 top_100 = df.sort_values("pontuacao", ascending=False).head(100)
-
 media_top_100 = top_100["pontuacao"].mean()
-
 print(f"Média geral: {media_geral:.2f}")
 print(f"Média top 100: {media_top_100:.2f}")
 
